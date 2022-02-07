96c628722840e6252f883c9d552f5913ca88034a
federico rufrancos <fde.ruf@gmail.com>
2022-02-07 10:10:12 -0300

    updating investments

--- backend/src/controllers/investmentController.js
+++ backend/src/controllers/investmentController.js
@@ -5,3 +5,17 @@ exports.getInvestments = async () => {
   const investments = await Investment.find();
   return investments;
 };
+
+exports.updateInvestments = async (updates) => {
+  return Promise.all(
+    updates.map(async (invest) => {
+      try {
+        const [investment] = await Investment.find({ name: invest.name });
+        investment.unitPrice = invest.unitPrice;
+        return await investment.save();
+      } catch (error) {
+        console.log('error al actualizer', error);
+      }
+    })
+  );
+};
--- backend/src/routes/investmentRoutes.js
+++ backend/src/routes/investmentRoutes.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const { getInvestments } = require('../controllers/investmentController');
+const {
+  getInvestments,
+  updateInvestments,
+} = require('../controllers/investmentController');
 const { INVESTMENT_DATA } = require('../data');
 const Investment = mongoose.model('Investment');
 
 router.get('/investments', async (req, res) => {
   const investments = await getInvestments();
   res.send(investments);
+});
+
+router.put('/investments', async (req, res) => {
+  const {
+    body: { updates },
+  } = req;
+  const response = await updateInvestments(updates);
+  res.status(200).send('acciones y bonos actualizados');
 });
 
 router.get('/create-investments', async (req, res) => {
