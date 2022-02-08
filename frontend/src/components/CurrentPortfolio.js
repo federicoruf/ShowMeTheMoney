527ebc76343d0170e2208301d4137e479d43a82f
federico rufrancos <fde.ruf@gmail.com>
2022-02-08 13:22:01 -0300

    adding pie graph

--- frontend/src/components/CurrentPortfolio.js
+++ frontend/src/components/CurrentPortfolio.js
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import PieGraph from './PieGraph';
 
 const CurrentPortfolio = ({ user, allInvestments }) => {
   const { savings, investments } = user;
   const [total, setTotal] = useState(0);
+  const [graphInvestments, setGraphInvestments] = useState([]);
 
   useEffect(() => {
     if (user && allInvestments && total === 0) {
       let totalAssets = savings;
 
+      setGraphInvestments([
+        ...graphInvestments,
+        { name: 'Caja de ahorro', type: '', value: savings },
+      ]);
+
       investments.forEach(({ name, type, units }) => {
         const data = allInvestments.get(name);
         const assetPrice = data.unitPrice * units;
+        setGraphInvestments((oldArray) => [
+          ...oldArray,
+          { name, type, value: assetPrice },
+        ]);
         totalAssets = totalAssets + assetPrice;
       });
       setTotal(total + totalAssets);
@@ -21,23 +32,10 @@ const CurrentPortfolio = ({ user, allInvestments }) => {
     return <></>;
   }
 
-  const calculateValue = (name, units) => {
-    return allInvestments.get(name).unitPrice * units;
-  };
-
   return (
     <div>
       <h1>Valor total cartera: AR$ {total}</h1>
-      <ul>
-        <li>Caja ahorro (AR$ {user.savings})</li>
-        {investments.map(({ name, units, type }) => {
-          return (
-            <li key={name}>
-              {type} {name} (AR$ {calculateValue(name, units)})
-            </li>
-          );
-        })}
-      </ul>
+      <PieGraph graphInvestments={graphInvestments} />
     </div>
   );
 };
---
+++ frontend/src/components/PieGraph.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { PieChart, Pie, Cell, Label } from 'recharts';
+
+const random = (min = 111, max = 999) => {
+  let num = Math.random() * (max - min) + min;
+
+  return Math.floor(num);
+};
+
+const PieGraph = ({ graphInvestments }) => {
+  const renderLabelContent = (props) => {
+    const { value, type, name, x, y, midAngle } = props;
+
+    return (
+      <g
+        transform={`translate(${x}, ${y})`}
+        textAnchor={midAngle < -90 || midAngle >= 90 ? 'end' : 'start'}
+        width={500}
+      >
+        <text x={0} y={0} width={500}>
+          {type} {name}
+        </text>
+        <text x={0} y={20}>{`AR$ ${value}`}</text>
+      </g>
+    );
+  };
+
+  return (
+    <PieChart width={800} height={400}>
+      <Pie
+        data={graphInvestments}
+        dataKey='value'
+        cx={400}
+        cy={100}
+        startAngle={180}
+        endAngle={-180}
+        innerRadius={60}
+        outerRadius={80}
+        label={renderLabelContent}
+        paddingAngle={5}
+        isAnimationActive={false}
+      >
+        {graphInvestments.map((entry, index) => (
+          <Cell key={`slice-${index}`} fill={`#${random()}`} />
+        ))}
+        <Label width={50} position='center'>
+          Cartera
+        </Label>
+      </Pie>
+    </PieChart>
+  );
+};
+
+export default PieGraph;
